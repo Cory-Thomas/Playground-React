8f0f3af6bbe4532ae641171a9c586f07b8e10828
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-09-05 18:34:27 -0400

    basic use of useReducer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useReducer } from 'react';
 import './App.css';
 
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'increment' :
+      return {count: state.count + 1};
+    case 'decrement' :
+      return {count: state.count - 1};
+    default: 
+      return state;
+  }
+};
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, {count: 0})
+
+  const increment = () => {
+    return dispatch({type: 'increment' })
+  }
+
+  const decrement = () => {
+    return dispatch({type: 'decrement'})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={decrement}>-</button>
+      <div className="App">
+        {state.count}
+      </div>
+      <button onClick={increment}>+</button>
+    </>
   );
-}
+};
 
 export default App;
