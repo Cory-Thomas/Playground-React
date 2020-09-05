2f49b057dd6bc82c77a8446d88e2d23c0b2526f6
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-09-05 18:55:20 -0400

    added a save functionality with the useReducer code

--- src/App.js
+++ src/App.js
@@ -4,24 +4,32 @@ import './App.css';
 const reducer = (state, action) => {
   switch(action.type) {
     case 'increment' :
-      return {count: state.count + 1};
+      return {...state, count: state.count + 1};
     case 'decrement' :
-      return {count: state.count - 1};
+      return {...state, count: state.count - 1};
+    case 'save' :
+      return {...state, save: state.count}
     default: 
       return state;
   }
 };
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, {count: 0})
+  const [state, dispatch] = useReducer( reducer, {count: 0, save: null} )
 
   return (
     <>
-      <button onClick={() => dispatch({type: 'decrement'})}>-</button>
+      <button onClick={() => dispatch( {type: 'decrement'} )}> - </button>
       <div>
         {state.count}
       </div>
-      <button onClick={() => dispatch({type: 'increment'})}>+</button>
+      <button onClick={() => dispatch( {type: 'increment'} )}> + </button>
+      <div>
+        <div>
+          Saved State: {state.save}
+        </div>
+        <button onClick={() => dispatch( {type: "save"} )}>Save</button>
+      </div>
     </>
   );
 };
